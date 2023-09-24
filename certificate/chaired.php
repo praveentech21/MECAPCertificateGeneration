eb802c626895cf21cf6c2f4916c3b3f44242a4e2
Sai Praveen <ravikumar_csd@srkrec.edu.in>
2023-09-24 23:44:00 +0530

    certificate generation setted shiva

--- certificate/chaired.php
+++ certificate/chaired.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL); 
 
-$im = imagecreatefrompng("spellbee.png");
+$im = imagecreatefrompng("chaired.png");
 // if there's an error, stop processing the page:
 if(!$im)
 {
@@ -18,7 +18,7 @@ $font = './roboto.ttf'; // Provide the correct path to your font file
 $rollno=($_POST['rollno']);
 include "../connect.php";
 
-$result=mysqli_query($conn, "SELECT * FROM `users` WHERE `pid`='$rollno'");
+$result=mysqli_query($conn, "SELECT * FROM `members` WHERE `mobile`='$rollno'");
 $row=mysqli_fetch_assoc($result);
 
 if(mysqli_num_rows($result) == 0)
@@ -28,43 +28,17 @@ if(mysqli_num_rows($result) == 0)
 else
   {
 	//echo "<h2 style='color:#AA0055;font-family:Arial;'>CODE MASTER 2018 - LEVEL 1 CERTIFICATE</h2>";  
-  $sname=ucwords(strtolower($row['player_name']));
-  $rollno=$row['regno'];
-  if($row['place'] == '2024') $year = '4'; 
-  elseif($row['place'] == '2025') $year = '3'; 
-  elseif($row['place'] == '2026') $year = '2'; 
-  elseif($row['place'] == '2027') $year = '1';
-
-  if($row['department'] == 'CSD') $department = 'Computer Science and Design';
-  elseif($row['department'] == 'CSE') $department = 'CSE';
-  elseif($row['department'] == 'ECE') $department = 'ECE';
-  elseif($row['department'] == 'EEE') $department = 'EEE';
-  elseif($row['department'] == 'MECH') $department = 'Mechanical Engineering';
-  elseif($row['department'] == 'CIVIL') $department = 'Civil Engineering';
-  elseif($row['department'] == 'IT') $department = 'Information Technology';
-  elseif($row['department'] == 'AIDS') $department = 'AIDS';
-  elseif($row['department'] == 'AIML') $department = 'AIML';
-  elseif($row['department'] == 'CSBS') $department = 'CSBS';
-  elseif($row['department'] == 'CST') $department = 'CSI';
-  elseif($row['department'] == 'CIC') $department = 'CIC';
-
+  $sname=ucwords($row['name']);
+  $subject=$row['department'];
   //$name=$sname." - ".$rollno;
-  $name=strtoupper($sname);
 
-    $batch=$year." /4 - ".$department;
-    
-	  
-  
 //writing name and roll number
-$text = $name;
-imagettftext($im, 20, 0, 470, 353, $black, $font, $text);
+$text = $sname;
+imagettftext($im, 30, 0, 570, 790, $black, $font, $text);
 
 //writing rool number
-$text = $batch; 
-imagettftext($im, 20, 0, 150, 413, $black, $font, $text);
-
-
-
+$text = $subject; 
+imagettftext($im, 30, 0, 320, 880, $black, $font, $text);
 
 $myfile = "tmp/".$rollno.".png";
 
